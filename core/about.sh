7cc0e1df37420bf27c5d9ca5acb74c4a969e3358
Scone-techh <75084404+scone-tech@users.noreply.github.com>
2020-11-26 20:07:34 +0100

    Create about.sh

---
+++ core/about.sh
@@ -0,0 +1,25 @@
+  
+clear
+echo '                    
+            __   __     _____   __       __       _____    
+           /\_\ /_/\  /\_____\ /\_\     /\_\     ) ___ (   
+          ( ( (_) ) )( (_____/( ( (    ( ( (    / /\_/\ \  
+           \ \___/ /  \ \__\   \ \_\    \ \_\  / /_/ (_\ \ 
+           / / _ \ \  / /__/_  / / /__  / / /__\ \ )_/ / / 
+          ( (_( )_) )( (_____\( (_____(( (_____(\ \/_\/ /  
+           \/_/ \_\/  \/_____/ \/_____/ \/_____/ )_____(   
+                                                 
+' | lolcat
+echo " "
+echo "                             About"|lolcat
+echo " "
+echo "       🙏 Hey, there I am Hacker(S C O N E), i made this tool
+  to penetrate password strength In termux, so i hope guys you
+                             liked it. 😘"
+echo ""
+echo "                  Our channel :- HACKERS"| lolcat
+echo " "
+
+sleep 5.0
+cd $HOME/instahack
+bash instahack.sh
